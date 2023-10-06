0d66dcca4b8e271b02006a935dc51ef60699bc1c
Alejandra Bedolla <alejandrabedolla@Blancas-MBP.fios-router.home>
2023-10-06 16:13:39 -0400

    last push

--- src/main/java/vehiculos/Main.java
+++ src/main/java/vehiculos/Main.java
@@ -5,6 +5,12 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+
+        // creating instance of plane, boat, car and truck
+        Plane plane = new Plane("Boeing","5000",50,000);
+        Boat boat = new Boat();
+        Car car = new Car();
+        Truck truck = new Truck();
         // create an Arraylist of Vehicles
         List<Vehicle> vehiclesList = new ArrayList<>();
 
